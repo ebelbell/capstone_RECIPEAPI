77dafea43992ad6ff0ccb25912b8a5c695a98d10
E Abella <eabella@Es-MacBook-Air.local>
2024-07-29 00:55:44 -0700

    created my data.js file for mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,14 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-
 //imports
-import axios from 'axios'; //receive and send requests to the backend
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
 
-const App =() => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const getData=async() => {
-      try {
-        const response=await axios.get('http://localhost:3000/recipe'); //request data from backend
-        setData(response.data);
-      } catch (err) {
-
-      }
-    };
-
-    getData();
-  }, []);
-
-  return (
-    <div>{data}</div>
-  )
-}
+const App = () => (
+    <Router>
+        <Routes>
+            <Route path="/" element = {
+              <RecipeList />} />
+        </Routes>
+    </Router>
+);
 
 export default App;
